f4f7785d752761150f0b200f22f24a4a0f41f042
MohamedPHP <mohamedzayed709@yahoo.com>
2017-05-28 19:44:12 +0200

    frist commit

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Responsive</title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!-- Start Header -->
+        <div class="header text-center parallax-window-header" data-parallax="scroll" data-image-src="images/laravel-banner.jpg">
+            <div class="overlay">
+                <div class="container">
+                    <h1>Laravel</h1>
+                    <hr>
+                    <div class="plan">
+                        Start Your Laravel Cooding To Day
+                    </div>
+                    <div class="plan">
+                        Start Your Laravel Cooding Tommorow
+                    </div>
+                    <a href="#" class="myButton">
+                        Download
+                    </a>
+                </div>
+            </div>
+        </div>
+        <!-- End Header -->
+
+        <!-- Start Features -->
+        <div class="features">
+            <div class="container">
+                <h2 class="h2 text-center">Our Features</h2>
+                <div class="feet fl">
+                    <h2 class="text-center">fast</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+                <div class="feet fl">
+                    <h2 class="text-center">fast</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+                <div class="feet fl">
+                    <h2 class="text-center">fast</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- End Featuries -->
+
+
+        <!-- about -->
+        <div class="about parallax-window" data-parallax="scroll" data-image-src="images/about.jpeg">
+            <div class="overlay">
+                <div class="container">
+                    <h1 class="h1 text-center">About</h1>
+                    <p class="res-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+        </div>
+        <!-- about -->
+
+        <!-- our-works -->
+        <div class="our-works">
+            <div class="image fl">
+                <img class="res-img" src="images/01.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/02.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/03.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/04.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/05.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/06.jpg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/7.jpeg" alt="">
+            </div>
+            <div class="image fl">
+                <img class="res-img" src="images/08.jpg" alt="">
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <!-- our-works -->
+
+        <!-- plans -->
+        <div class="plans" data-parallax="scroll" data-image-src="images/plans.jpg">
+            <div class="container">
+                <h1 class="h2 text-center">Plans</h1>
+                <section id="pricePlans">
+                    <ul id="plans">
+                        <li class="plan">
+                            <ul class="planContainer">
+                                <li class="title"><h2>Plan 1</h2></li>
+                                <li class="price"><p>$10/<span>month</span></p></li>
+                                <li>
+                                    <ul class="options">
+                                        <li>2x <span>option 1</span></li>
+                                        <li>Free <span>option 2</span></li>
+                                        <li>Unlimited <span>option 3</span></li>
+                                        <li>Unlimited <span>option 4</span></li>
+                                        <li>1x <span>option 5</span></li>
+                                    </ul>
+                                </li>
+                                <li class="button"><a href="#">Purchase</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="plan">
+                            <ul class="planContainer">
+                                <li class="title"><h2 class="bestPlanTitle">Plan 2</h2></li>
+                                <li class="price"><p class="bestPlanPrice">$20/month</p></li>
+                                <li>
+                                    <ul class="options">
+                                        <li>2x <span>option 1</span></li>
+                                        <li>Free <span>option 2</span></li>
+                                        <li>Unlimited <span>option 3</span></li>
+                                        <li>Unlimited <span>option 4</span></li>
+                                        <li>1x <span>option 5</span></li>
+                                    </ul>
+                                </li>
+                                <li class="button"><a class="bestPlanButton" href="#">Purchase</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="plan">
+                            <ul class="planContainer">
+                                <li class="title"><h2>Plan 3</h2></li>
+                                <li class="price"><p>$30/<span>month</span></p></li>
+                                <li>
+                                    <ul class="options">
+                                        <li>2x <span>option 1</span></li>
+                                        <li>Free <span>option 2</span></li>
+                                        <li>Unlimited <span>option 3</span></li>
+                                        <li>Unlimited <span>option 4</span></li>
+                                        <li>1x <span>option 5</span></li>
+                                    </ul>
+                                </li>
+                                <li class="button"><a href="#">Purchase</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="plan">
+                            <ul class="planContainer">
+                                <li class="title"><h2>Plan 4</h2></li>
+                                <li class="price"><p>$40/<span>month</span></p></li>
+                                <li>
+                                    <ul class="options">
+                                        <li>2x <span>option 1</span></li>
+                                        <li>Free <span>option 2</span></li>
+                                        <li>Unlimited <span>option 3</span></li>
+                                        <li>Unlimited <span>option 4</span></li>
+                                        <li>1x <span>option 5</span></li>
+                                    </ul>
+                                </li>
+                                <li class="button"><a href="#">Purchase</a></li>
+                            </ul>
+                        </li>
+                    </ul> <!-- End ul#plans -->
+                </section>
+            </div>
+        </div>
+        <!-- plans -->
+
+        <!-- testmonials -->
+        <div class="they-say">
+            <div class="container">
+                <h1 class="text-center h1">What They Say About Us</h1>
+                <div class="person-say">
+                    <h3>Ahmed</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="person-say fl">
+                    <h3>Sayed</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="person-say fl">
+                    <h3>Mahmoud</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- testmonials -->
+
+        <!-- info -->
+        <div class="info parallax-window-info" data-parallax="scroll" data-image-src="images/info.jpg">
+            <div class="overlay">
+                <div class="container">
+                    <h1 class="h1 text-center">Information</h1>
+                    <ul class="classic-list info-list">
+                        <li data-class="about-info"  class="active">About</li>
+                        <li data-class="products" >Products</li>
+                        <li data-class="test" >Test</li>
+                        <li data-class="proto" >Proto</li>
+                    </ul>
+                    <div class="clearfix"></div>
+                    <div class="info-content">
+                        <div class="about-info">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="products">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="test">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="proto">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- info -->
+
+        <!-- footer -->
+        <footer>
+            <div class="container">
+                <div class="copy fl">
+                    Copy Ritht To Mohamed Zayed
+                </div>
+                <div class="design fl">
+                    Designed By: Mohamed Zayed Mohamed
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </footer>
+        <!-- footer -->
+
+
+
+        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script type="text/javascript">
+            $('.parallax-window').parallax({imageSrc: "images/about.jpeg"});
+            $('.parallax-window-header').parallax({imageSrc: "images/laravel-banner.jpg"});
+            $('.parallax-window-info').parallax({imageSrc: "images/info.jpg"});
+            $('.plans').parallax({imageSrc: "images/plans.jpg"});
+            $(document).on('click', '.myButton', function(event) { event.preventDefault(); });
+            $(document).on('click', '.info-list li', function() {
+                $(this).addClass('active').siblings('li').removeClass('active');
+                $('.info-content div').hide();
+                $('.info-content ' + '.' + $(this).data('class')).fadeIn(900);
+            });
+        </script>
+    </body>
+</html>
